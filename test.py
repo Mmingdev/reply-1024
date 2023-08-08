f0c89a5d74b74fcc5560881cdaf644a259a7203a
“Mmingdev” <stormcar@dingtalk.com>
2023-08-08 15:41:54 +0800

    测试

--- test.py
+++ test.py
@@ -4,15 +4,19 @@ from reply1024 import postreply1024
 # import time
 from datetime import datetime, timedelta
 
+APP_ID = os.getenv('APP_ID')
+APP_SECRET = os.getenv('APP_SECRET')
+USER_ID = os.getenv('USER_ID')
+TEMPLATE_ID = os.getenv('TEMPLATE_ID')
 time1 = datetime.now() + timedelta(hours=8)
 # time1 = time1.strftime("%Y-%m-%d %H:%M:%S")
-time2=datetime(time1.year,time1.month,time1.day,15,20,0,0)
+time2=datetime(time1.year,time1.month,time1.day,15,55,0,0)
 n=(time2-time1).seconds
 print(f'time1={time1},type={type(time1)}')
 print(f'time2={time2},type={type(time2)}')
 print(f"time2-time1={n}")
 sleep(n)
 time3 = datetime.now() + timedelta(hours=8)
-cl1=postreply1024('','','','','','','')
+cl1 = postreply1024('', '', APP_ID, APP_SECRET, USER_ID, TEMPLATE_ID, '')
 msg=f'{time1}~{time2}~{time3}'
 cl1._send_to_mp(msg)
