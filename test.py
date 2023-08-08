1afcc0f02b752ceefbae7c3c240b449b8814dbe0
“Mmingdev” <stormcar@dingtalk.com>
2023-08-08 16:04:29 +0800

    测试

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+import os
 from reply1024 import postreply1024
 # import time
 from datetime import datetime, timedelta
@@ -10,7 +10,7 @@ USER_ID = os.getenv('USER_ID')
 TEMPLATE_ID = os.getenv('TEMPLATE_ID')
 time1 = datetime.now() + timedelta(hours=8)
 # time1 = time1.strftime("%Y-%m-%d %H:%M:%S")
-time2=datetime(time1.year,time1.month,time1.day,15,55,0,0)
+time2=datetime(time1.year,time1.month,time1.day,16,10,0,0)
 n=(time2-time1).seconds
 print(f'time1={time1},type={type(time1)}')
 print(f'time2={time2},type={type(time2)}')
