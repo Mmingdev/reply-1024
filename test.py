7e61b9e6042422e87b32c4eb7df4ca36da033d38
Mmingdev <stormcar@dingtalk.com>
2025-08-11 21:47:33 +0800

    测试

--- test.py
+++ test.py
@@ -4,23 +4,29 @@ import os
 # import time
 from datetime import datetime, timedelta
 
-tday = datetime.now()+timedelta(hours = 8)
-print(tday.hour)
-tday.strftime("%Y-%m-%d %H:%M:%S")
-print(tday)
-if os.path.isdir("tmp")==0:
-    os.mkdir("tmp")
+APP_ID = os.getenv('APP_ID')
+APP_SECRET = os.getenv('APP_SECRET')
+USER_ID = os.getenv('USER_ID')
+TEMPLATE_ID = os.getenv('TEMPLATE_ID')
 
-with open("./tmp/test.txt","r+") as f:
-    # f.write(f"{tday} 成功添加一行内容\n")
-    con=f.read()
-    a=60
-    f.seek(0)
-    f.truncate()
-    f.write(f"{int(con)+a}")
-# print(f"文件关闭了吗：{f.closed}")
-with open("./tmp/test.txt") as f:
-    con=f.read()
-
-# print(f"文件关闭了吗：{f.closed}")
-print("读取内容：",con)
+print('{},{},{},{}'.format(APP_ID,APP_SECRET,USER_ID,TEMPLATE_ID))
+# tday = datetime.now()+timedelta(hours = 8)
+# print(tday.hour)
+# tday.strftime("%Y-%m-%d %H:%M:%S")
+# print(tday)
+# if os.path.isdir("tmp")==0:
+#     os.mkdir("tmp")
+#
+# with open("./tmp/test.txt","r+") as f:
+#     # f.write(f"{tday} 成功添加一行内容\n")
+#     con=f.read()
+#     a=60
+#     f.seek(0)
+#     f.truncate()
+#     f.write(f"{int(con)+a}")
+# # print(f"文件关闭了吗：{f.closed}")
+# with open("./tmp/test.txt") as f:
+#     con=f.read()
+#
+# # print(f"文件关闭了吗：{f.closed}")
+# print("读取内容：",con)
