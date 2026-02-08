4b7e09e2e467d46b82ad4410b6023d05a9dc9a1e
Mmingdev <stormcar@dingtalk.com>
2026-02-08 23:39:45 +0800

    更新时间，调整时间更新代码顺序

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ if __name__ == '__main__':
     lday = lastupdate.day
     now = datetime.now() + timedelta(hours=8)
     this_month_end = calendar.monthrange(now.year, now.month)[1]
-    lastupdate = lastupdate + timedelta(days=1)
-    w_content = "lastupdate:" + datetime.strftime(lastupdate,"%Y-%m-%d")
-    with open("tmp/temp.txt","w") as file:
-        file.write(w_content)
     if lday != this_month_end:
         reply1024()
     else:
         print("已到当月最后一天，明天需要更新")
+    lastupdate = lastupdate + timedelta(days=1)
+    w_content = "lastupdate:" + datetime.strftime(lastupdate, "%Y-%m-%d")
+    with open("tmp/temp.txt", "w") as file:
+        file.write(w_content)
